5f18a85e94fbf490845c4c76766ae4271e2915ac
lazy-sky <1cielo@naver.com>
2023-06-17 22:11:55 +0900

    mongoose connection
    id, pw: mernuser

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8888
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://mernuser:mernuser@mernboilerplate.btocojd.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB Connected!')
+}).catch((error) => {
+  console.log(`MongoDB connection Error: ${error}`)
+})
+
 app.get('/', (req, res) => res.send('Hello World!!!'))
 
 app.listen(port, () => {
